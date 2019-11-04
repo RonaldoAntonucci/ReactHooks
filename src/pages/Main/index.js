a6de452f1d6ca289e79eb41bdfe798b665fa690c
RonaldoAntonucci <ronaldo.antonuccivrb32@yahoo.com.br>
2019-11-04 15:13:18 -0300

    aula completa

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,46 @@
-import React from 'react';
-
-import { Title } from './styles';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
 export default function Main() {
-  return <Title>Hellow World</Title>;
+  const [techs, setTechs] = useState([]);
+  const [newTech, setNewTech] = useState('');
+
+  useEffect(() => {
+    const storageTechs = localStorage.getItem('techs');
+
+    if (storageTechs) {
+      setTechs(JSON.parse(storageTechs));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('techs', JSON.stringify(techs));
+  }, [techs]);
+
+  const techsSize = useMemo(() => techs.length, [techs]);
+
+  const handleAddTech = useCallback(() => {
+    setTechs([...techs, newTech]);
+    setNewTech('');
+  }, [newTech, techs]);
+
+  return (
+    <>
+      <ul>
+        {techs.map(tech => (
+          <li key={tech}>{tech}</li>
+        ))}
+      </ul>
+      <br />
+      <strong>Você tem {techsSize} Techs</strong>
+      <br />
+      <input
+        value={newTech}
+        onChange={e => setNewTech(e.target.value)}
+        type="text"
+      />
+      <button type="button" onClick={handleAddTech}>
+        Add
+      </button>
+    </>
+  );
 }
